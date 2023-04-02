cd8d7fe2bd89e53ce4617bc33554f48dfc487104
sergeybryan <regkmastis@yandex.ru>
2023-04-02 22:06:43 +0300

    Домашняя работа по Junit

---
+++ src/main/java/com/example/testing_homework_junit/TestingHomeworkJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_homework_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingHomeworkJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingHomeworkJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testing_homework_junit/TestingHomeworkJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_homework_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingHomeworkJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
